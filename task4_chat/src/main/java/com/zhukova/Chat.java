a9485e24e4f3d432a5dcac3af045f60aa2505969
CHRNV <zhukova.galina@bk.ru>
2016-05-13 01:31:21 +0300

    COMMIT1

--- task4_chat/src/main/java/com/zhukova/Chat.java
+++ task4_chat/src/main/java/com/zhukova/Chat.java
@@ -10,7 +10,28 @@ public class Chat extends HttpServlet {
     @Override
     //метод возвращает список всех обращений
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = null;
 
+        try {
+            out = response.getWriter();
+            fReader(out);
+        } catch (IOException ignored) {
+        }
+    }
+
+    private void fReader(PrintWriter out) throws IOException {
+        String path = "D:\\GALINA\\УЧЕБА\\3 КУРС\\ПРОГРАММИРОВАНИЕ WOW\\github\\java_oh\\task4_chat\\src\\main\\webapp\\WEB-INF\\index.html";
+        InputStream is = new FileInputStream(new File(path));
+        Reader r = new InputStreamReader(is, "utf-8");
+        BufferedReader br = new BufferedReader(r);
+        while (true) {
+            String s = br.readLine();
+            if (s == null)
+                break;
+            out.println(s);
+        }
+        is.close();
     }
 
     @Override
